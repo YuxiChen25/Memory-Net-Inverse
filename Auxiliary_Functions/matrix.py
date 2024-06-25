ca7071037d10158bfa315132ce0af0c462d6d17e
Eric Chen <wizard@Erics-MacBook-Pro-2.local>
2024-06-25 12:39:02 -0400

    new sampling matrices and resolution

--- Auxiliary_Functions/matrix.py
+++ Auxiliary_Functions/matrix.py
@@ -61,6 +61,33 @@ def kron_product_1d_dct(n: int) -> np.ndarray:
     D = dct_matrix(n)
     return np.kron(D, D)
 
+def generate_103_idxs(n: int) -> List[int]:
+    """
+    Generates a list of 103 indices located on the top left corner
+    of an image for the subsampling matrix in the DCT setting. 
+
+    Parameters
+    ----------
+    n : int
+        The length of the image (assumed to be square).
+
+    Returns
+    -------
+    List[int]
+        A list of indices 
+    """
+
+    indices = []
+
+    for col in range(10):
+        for row in range(10):
+            indices.append(row * n + col)
+
+    for col in range(3):
+        indices.append(10 * n + col)
+
+    return indices
+
 def generate_250_idxs(n: int) -> List[int]:
     """
     Generates a list of 250 indices located on the top left corner
@@ -74,7 +101,7 @@ def generate_250_idxs(n: int) -> List[int]:
     Returns
     -------
     List[int]
-        A list of indices for a 16x16 grid in a zigzag pattern.
+        A list of indices
     """
 
     indices = []
@@ -90,9 +117,80 @@ def generate_250_idxs(n: int) -> List[int]:
 
     return indices
 
-def create_sampling_matrix(n: int, k: int, seed: int = None) -> np.ndarray:
+def generate_410_idxs(n: int) -> List[int]:
     """
-    Creates a sampling matrix based on fixed indices and randomly sampled additional indices.
+    Generates a list of 410 indices located on the top left corner
+    of an image for the subsampling matrix in the DCT setting. 
+
+    Parameters
+    ----------
+    n : int
+        The length of the image (assumed to be square).
+
+    Returns
+    -------
+    List[int]
+        A list of indices 
+    """
+
+    indices = []
+
+    for col in range(20):
+        for row in range(20):
+            indices.append(row * n + col)
+
+    for col in range(10):
+        indices.append(20 * n + col)
+
+    return indices
+
+def create_sampling_matrix_32(n: int, k: int, seed: int = None) -> np.ndarray:
+    """
+    Creates a DCT sampling matrix based on fixed indices and
+    randomly sampled additional indices for 32 x 32 images
+
+    Parameters
+    ----------
+    n : int
+        The length of the image (assumed to be square).
+    k : int
+        The total number of indices to include in the sampling matrix.
+    seed : int, optional
+        The seed value for the random number generator. If not provided, a random seed will be used.
+
+    Returns
+    -------
+    np.ndarray
+        The sampling matrix of size total_k x (n^2).
+    """
+    
+    total_pixels = n * n
+
+    if k > total_pixels:
+        raise ValueError("total_k must be less than or equal to total pixels in the image")
+    
+    S = np.zeros((k, total_pixels))
+    fixed_idxs = generate_103_idxs(n)
+    other_idxs = set(range(total_pixels)) - set(fixed_idxs)
+
+    rng = np.random.default_rng(seed)
+    
+    if k > 103:
+        extra_idxs = rng.choice(list(other_idxs), k - 103, replace=False).tolist()
+    else:
+        extra_idxs = []
+    
+    all_idxs = sorted(fixed_idxs + extra_idxs)
+    
+    for i, index in enumerate(all_idxs):
+        S[i, index] = 1
+
+    return S
+
+def create_sampling_matrix_50(n: int, k: int, seed: int = None) -> np.ndarray:
+    """
+    Creates a DCT sampling matrix based on fixed indices and
+    randomly sampled additional indices for 50 x 50 images
 
     Parameters
     ----------
@@ -122,6 +220,49 @@ def create_sampling_matrix(n: int, k: int, seed: int = None) -> np.ndarray:
     
     if k > 250:
         extra_idxs = rng.choice(list(other_idxs), k - 250, replace=False).tolist()
+    else:
+        extra_idxs = []
+    
+    all_idxs = sorted(fixed_idxs + extra_idxs)
+    
+    for i, index in enumerate(all_idxs):
+        S[i, index] = 1
+
+    return S
+
+def create_sampling_matrix_64(n: int, k: int, seed: int = None) -> np.ndarray:
+    """
+    Creates a DCT sampling matrix based on fixed indices and
+    randomly sampled additional indices for 64 x 64 images
+
+    Parameters
+    ----------
+    n : int
+        The length of the image (assumed to be square).
+    k : int
+        The total number of indices to include in the sampling matrix.
+    seed : int, optional
+        The seed value for the random number generator. If not provided, a random seed will be used.
+
+    Returns
+    -------
+    np.ndarray
+        The sampling matrix of size total_k x (n^2).
+    """
+    
+    total_pixels = n * n
+
+    if k > total_pixels:
+        raise ValueError("total_k must be less than or equal to total pixels in the image")
+    
+    S = np.zeros((k, total_pixels))
+    fixed_idxs = generate_410_idxs(n)
+    other_idxs = set(range(total_pixels)) - set(fixed_idxs)
+
+    rng = np.random.default_rng(seed)
+    
+    if k > 410:
+        extra_idxs = rng.choice(list(other_idxs), k - 410, replace=False).tolist()
     else:
         extra_idxs = []
     
